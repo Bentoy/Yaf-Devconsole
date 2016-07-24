837c935d052649a3440bab1655265e279adaf5d9
Nonito Bano <Nonito Bano>
2016-07-24 18:32:02 +0800

    my initial commit

---
+++ DevConsole/OrderAscending.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevConsole
+{
+    public class MathFunctions
+    {
+        public int[] OrderAscending(int[] numbers)
+        {
+            int[] ordered = new int[6];
+
+            Console.WriteLine("Unordered list");
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                int next = index + 1;
+                if (next == numbers.Length)
+                    continue;
+
+                if (numbers[index] > numbers[index + 1])
+                {
+                    int temp = numbers[index];
+                    numbers[index] = numbers[index + 1];
+                    numbers[index + 1] = temp;
+                }
+            }
+
+            Console.WriteLine("\nFirst iteration list");
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+
+            return ordered;
+        }
+    }
+}
---
+++ DevConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevConsole
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MathFunctions function = new MathFunctions();
+            function.OrderAscending(new int[] { 1, 10, 7, 3, 9, 11 });
+
+
+            Console.ReadLine();
+        }
+    }
+}
